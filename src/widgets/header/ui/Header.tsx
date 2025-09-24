51bc1f565b12f26a1dbee3fd8ff51e5c794da9af
Azizbek Programmer <azizbekmirzavaliyev31@gmail.com>
2025-09-24 09:24:49 +0500

    first commit

---
+++ src/widgets/header/ui/Header.tsx
@@ -0,0 +1,11 @@
+import { memo } from "react"
+
+export const Header = memo(() => {
+  return (
+    <header>
+      <div className="container">
+        Header
+      </div>
+    </header>
+  )
+})
