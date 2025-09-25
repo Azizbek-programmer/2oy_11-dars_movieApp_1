ed7f197cf77713eb97095f114712bc7f86aca767
Azizbek Programmer <azizbekmirzavaliyev31@gmail.com>
2025-09-25 06:52:47 +0500

    davom

--- src/pages/movie/ui/Movie.tsx
+++ src/pages/movie/ui/Movie.tsx
@@ -1,25 +1,23 @@
 import { useMovie } from "@/entities/movie";
 import { MoviePagination } from "@/features/movie-pagination";
 import { MovieList } from "@/widgets/movie-list";
-import { memo, useState } from "react";
+import { memo } from "react";
 import { useSearchParams } from "react-router-dom";
 import { MovieCategoryFilter } from "@/features/movieFilter/ui/MovieCategoryFilter";
-import MovieSort from "@/features/movieSort";
+import { MovieSort } from "@/features/movieSort/ui/MovieSort";
 
 export const Movie = memo(() => {
   const { getMovies } = useMovie();
   const [searchParams, setSearchParams] = useSearchParams();
-  const [selectedGenre, setSelectedGenre] = useState<number | undefined>();
-  
-  
 
   const page = searchParams.get("page") ?? "1";
   const sort_by = searchParams.get("sort_by") ?? "popularity.desc";
+  const category = searchParams.get("category");
 
   const { data } = getMovies({
-    page: page as string,
-    with_genres: selectedGenre ? selectedGenre.toString() : undefined,
-    sort_by: sort_by
+    page: page,
+    sort_by: sort_by,
+    with_genres: category ? category : undefined,
   });
 
   const handlePageChange = (newPage: number) => {
@@ -27,24 +25,21 @@ export const Movie = memo(() => {
     setSearchParams(searchParams);
   };
 
-  const handleGenreChange = (genreId: number | undefined) => {
-    setSelectedGenre(genreId);
-    searchParams.set("page", "1");
-    setSearchParams(searchParams);
-  };
-
   return (
     <div>
-      <div className="container py-4 flex justify-start gap-8">
-        <MovieCategoryFilter value={selectedGenre} onChange={handleGenreChange} />
-        <MovieSort/>
+      <div className="container py-4 flex gap-4">
+        <MovieCategoryFilter
+          value={category ? Number(category) : undefined}
+        />
+        <MovieSort />
       </div>
+
       <MovieList movies={data?.results} />
 
-      <MoviePagination 
-        page={page} 
-        total_pages={data?.total_pages} 
-        onChange={handlePageChange} 
+      <MoviePagination
+        page={page}
+        total_pages={data?.total_pages}
+        onChange={handlePageChange}
       />
     </div>
   );
