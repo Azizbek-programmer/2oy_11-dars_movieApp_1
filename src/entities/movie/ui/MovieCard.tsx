20a1fd8fea4288a6482dae781ce5c7ec72db6f87
Azizbek Programmer <azizbekmirzavaliyev31@gmail.com>
2025-10-23 10:51:48 +0500

    fial

--- src/entities/movie/ui/MovieCard.tsx
+++ src/entities/movie/ui/MovieCard.tsx
@@ -4,18 +4,16 @@ import type { IMovie } from "../model/types";
 import { useNavigate } from "react-router-dom";
 import MovieCardSkeleton from "./skeleton/MovieSkeleton";
 import { Bookmark } from "lucide-react";
-import type { RootState } from '../../../app/store';
-import { toggleCart } from '@/app/store/cartSlice';
-import { useDispatch, useSelector,  } from "react-redux";
-
+import type { RootState } from "@/app/store";
+import { toggleCart } from "@/app/store/cartSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 interface Props {
   movie?: IMovie;
-  isLoading?: boolean; 
+  isLoading?: boolean;
 }
 
-export const MovieCard: FC<Props> = memo((props) => {
-  const { movie, isLoading } = props;
+export const MovieCard: FC<Props> = memo(({ movie, isLoading }) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const cart = useSelector((state: RootState) => state.cart.value);
@@ -37,37 +35,38 @@ export const MovieCard: FC<Props> = memo((props) => {
           alt={movie.title}
           className="w-full h-[320px] object-cover rounded-t-xl"
         />
+
+        {/* Release Year */}
         <span className="absolute top-2 left-2 bg-red-700/70 text-white text-xs font-semibold px-2 py-1 rounded-md">
           {movie.release_date.split("-")[0]}
         </span>
 
+        {/* Bookmark Icon */}
         <button
-          className="absolute top-2 right-2 p-2 rounded-full bg-black/50 text-white
-          opacity-100 sm:opacity-0 sm:group-hover:opacity-100 transition"
-           onClick={(e) => {
+          className="absolute top-2 right-2 p-2 rounded-full bg-black/50 text-white opacity-100 sm:opacity-0 sm:group-hover:opacity-100 transition"
+          onClick={(e) => {
             e.stopPropagation();
             dispatch(toggleCart(movie));
           }}
         >
-          <Bookmark size={20}
-          color={isAdded ? "#eab308" : "#ffffff"}
+          <Bookmark
+            size={20}
+            color={isAdded ? "#eab308" : "#ffffff"}
             fill={isAdded ? "#eab308" : "none"}
-             />
+          />
         </button>
       </div>
 
+      {/* Movie Info */}
       <div className="p-3 space-y-1">
-        <h3
-          className="line-clamp-1 font-semibold text-lg"
-          title={movie.title}
-        >
+        <h3 className="line-clamp-1 font-semibold text-lg" title={movie.title}>
           {movie.title}
         </h3>
         <p className="text-sm text-gray-500 dark:text-gray-400">
-          {movie.original_language}
+          {movie.original_language.toUpperCase()}
         </p>
         <p className="text-yellow-500 font-medium">
-          {movie.vote_average}
+          ⭐ {movie.vote_average.toFixed(1)}
         </p>
       </div>
     </div>
